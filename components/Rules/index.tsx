1a5faf8120664debefd95b55f9d6e88c8873d452
AbdullahHamadax <abdallahamada114@gmail.com>
2024-12-08 21:06:44 +0200

    Fixes and improvements

--- components/Rules/index.tsx
+++ components/Rules/index.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
-
 const Rules = () => {
   return (
     <main className="flex min-h-screen items-center justify-center bg-cDark">
       <div
         className="relative flex w-[30rem] flex-col gap-8
-      rounded-[2.5rem] bg-white p-8 sm:border-[3px]
-      sm:border-black sm:px-8 sm:shadow-custom"
+  rounded-[2.5rem] bg-white p-8 sm:border-[3px]
+  sm:border-black sm:px-8 sm:shadow-custom"
       >
-        <h1 className="f-1 text-center">RULES</h1>
+        <h1 className="f-l text-center">RULES</h1>
         <div className="flex flex-col gap-4">
           <h2 className="f-m text-cDark">OBJECTIVE</h2>
           <p>
@@ -22,20 +21,19 @@ const Rules = () => {
           <ol className="ol">
             <li className="li">Red goes first in the first game.</li>
             <li className="li">
-              Players must alternate turns, and only on disc can be dropped in
+              Players must alternate turns, and only one disc can be dropped in
               each turn.
             </li>
             <li className="li">
               The game ends when there is a 4-in-a-row or a stalemate.
             </li>
             <li className="li">
-              The starter of the previous game foes second on the next game.
+              The starter of the previous game goes second on the next game.
             </li>
           </ol>
         </div>
-
         <Link
-          className="absolute bottom-0 left-1/2 translate-x-[-50] translate-y-1/2"
+          className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"
           href="/"
         >
           <Image src="images/check.svg" alt="Confirm" width={64} height={64} />
