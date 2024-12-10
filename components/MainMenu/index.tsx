2bcaec60844ed295cc24c3c8f711815f7a976b43
Nour elden <noreldenahmed21@gmail.com>
2024-12-10 20:29:42 +0200

    Created and set up the app context file. Finished the board design of the ingame page for all screen sizes.

--- components/MainMenu/index.tsx
+++ components/MainMenu/index.tsx
@@ -6,15 +6,16 @@ const MainMenu = () => {
     <main className="flex min-h-screen items-center justify-center bg-cDark">
       <div
         className="flex h-[27.1875rem] w-[30rem] flex-col items-center
-            gap-8 rounded-[2.5rem] p-4 sm:border-[3px] sm:border-black sm:bg-cLight
-            sm:px-8 sm:shadow-custom"
+      gap-[2rem] rounded-[2.5rem] px-[1rem] py-[1rem] sm:border-[3px] sm:border-black
+      sm:bg-cLight sm:px-[2rem] sm:shadow-custom"
       >
         <Image src="images/logo.svg" alt="logo" width={52} height={52} />
-        <div className="relative flex size-full flex-col items-center">
-          <button
-            className="f-m absolute flex h-[4.5rem] w-full max-w-[25rem] items-center 
-            justify-between rounded-[1.25rem] border-[3px] border-black bg-cYellow p-5
-            text-left shadow-custom active:me-[0.3125rem] active:shadow-customActive"
+        <div className="relative flex h-full w-full flex-col items-center">
+          <Link
+            href="/ingame"
+            className="f-m absolute flex h-[4.5rem] w-full max-w-[25rem] items-center justify-between
+            rounded-[1.25rem] border-[3px] border-black bg-cYellow p-[1.25rem]
+            text-left shadow-custom active:mt-[0.3125rem] active:shadow-customActive"
           >
             PLAY VS PLAYER
             <Image
@@ -23,11 +24,13 @@ const MainMenu = () => {
               width={48}
               height={48}
             />
-          </button>
-          <button
+          
+          </Link>
+          <Link
+            href="/ingame"
             className="f-m absolute top-1/3 flex h-[4.5rem] w-full max-w-[25rem] items-center justify-between
-                 rounded-[1.25rem] border-[3px] border-black bg-cRed p-5
-                 text-left shadow-custom active:mt-[0.3125rem] active:shadow-customActive"
+            rounded-[1.25rem] border-[3px] border-black bg-cRed p-[1.25rem]
+            text-left shadow-custom active:mt-[0.3125rem] active:shadow-customActive"
           >
             PLAY VS CPU
             <Image
@@ -36,11 +39,11 @@ const MainMenu = () => {
               width={48}
               height={48}
             />
-          </button>
+          </Link>
           <Link
             className="f-m absolute top-2/3 h-[4.5rem] w-full max-w-[25rem]
-                    rounded-[1.25rem] border-[3px] border-black bg-white p-5
-                    text-left shadow-custom active:mt-[0.3125rem] active:shadow-customActive"
+           rounded-[1.25rem] border-[3px] border-black bg-white p-[1.25rem]
+           text-left shadow-custom active:mt-[0.3125rem] active:shadow-customActive"
             href="/rules"
           >
             GAME RULES
